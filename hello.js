7fed6c64987d8afbd5d941f1cdf096af6e2646ce
softageb <surajb.softage@gmail.com>
2023-11-07 08:52:24 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
 console.log('Pull request for new feature')
 console.log('Assigning pull request to specific member')
 console.log('Squash and merge pull request')
+console.log('Rebase and merge method ')
